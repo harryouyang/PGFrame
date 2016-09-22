cca2d76781daea6458b2eedffeabc11867ac8a44
harryouyang <oyh@ouyanghuademacbook-pro.local>
2016-09-22 09:59:16 +0800

    添加注释

--- panggu/logic/PGApp.h
+++ panggu/logic/PGApp.h
@@ -10,10 +10,19 @@
 
 @interface PGApp : NSObject
 
+/*
+ 设置NavBar属性
+ */
 + (void)configAppNavBar;
 
+/*
+ 获取App Window的最底层ViewController
+ */
 + (UIViewController *)appRootController;
 
+/*
+ 添加任务到主线程
+ */
 + (void)asyncOnMainQueue:(dispatch_block_t)block;
 
 @end
