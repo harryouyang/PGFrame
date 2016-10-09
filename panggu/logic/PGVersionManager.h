233fd8839610c19e32b9d24f09ae25a6ff631860
harryouyang <oyh@ouyanghuademacbook-pro.local>
2016-10-09 10:19:24 +0800

    添加版本更新管理

---
+++ panggu/logic/PGVersionManager.h
@@ -0,0 +1,13 @@
+//
+//  PGVersionManager.h
+//  PGFrame
+//
+//  Created by ouyanghua on 16/10/9.
+//  Copyright © 2016年 pangu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PGVersionManager : NSObject
+
+@end
---
+++ panggu/logic/PGVersionManager.m
@@ -0,0 +1,17 @@
+//
+//  PGVersionManager.m
+//  PGFrame
+//
+//  Created by ouyanghua on 16/10/9.
+//  Copyright © 2016年 pangu. All rights reserved.
+//
+
+#import "PGVersionManager.h"
+
+@interface PGVersionManager ()
+@property(nonatomic, assign)BOOL needUpdate;
+@end
+
+@implementation PGVersionManager
+
+@end
